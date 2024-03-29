e108e4cf6b9ba6114f5720420832db9afc08c526
ysh9698 <75485765+ysh9698@users.noreply.github.com>
2024-03-29 15:18:54 +0900

    좋아요, 댓글 기능 구현

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,11 +1,13 @@
 import Loader from "components/loader/Loader";
 import PostBox from "components/posts/PostBox";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 import { db } from "firebaseApp";
 import { PostProps } from "pages/home";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PostHeader from "components/posts/Header";
+import CommentForm from "components/comments/CommentForm";
+import CommentBox, { CommentProps } from "components/comments/CommentBox";
 
 export default function PostDetail() {
   const params = useParams();
@@ -14,9 +16,9 @@ export default function PostDetail() {
   const getPost = useCallback(async () => {
     if (params.id) {
       const docRef = doc(db, "posts", params.id);
-      const docSnap = await getDoc(docRef);
-
-      setPost({ ...(docSnap?.data() as PostProps), id: docSnap.id });
+      onSnapshot(docRef, (doc) => {
+        setPost({ ...(doc?.data() as PostProps), id: doc.id });
+      });
     }
   }, [params.id]);
 
@@ -27,7 +29,20 @@ export default function PostDetail() {
   return (
     <div className="post">
       <PostHeader />
-      {post ? <PostBox post={post} /> : <Loader />}
+      {post ? (
+        <>
+          <PostBox post={post} />
+          <CommentForm post={post} />
+          {post?.comments
+            ?.slice(0)
+            ?.reverse()
+            ?.map((data: CommentProps, index: number) => (
+              <CommentBox data={data} key={index} post={post} />
+            ))}
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -13,27 +13,43 @@ import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const PROFILE_DEFAULT_URL = "/logo512.png";
+type TabType = "my" | "like";
 
 export default function ProfilePage() {
-  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [activeTab, setActiveTab] = useState<TabType>("my");
+  const [myPosts, setMyPosts] = useState<PostProps[]>([]);
+  const [likePosts, setLikePosts] = useState<PostProps[]>([]);
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
     if (user) {
       let postsRef = collection(db, "posts");
-      let postsQuery = query(
+      const myPostsQuery = query(
         postsRef,
         where("uid", "==", user.uid),
         orderBy("createdAt", "desc")
       );
+      const likePostsQuery = query(
+        postsRef,
+        where("likes", "array-contains", user.uid),
+        orderBy("createdAt", "desc")
+      );
 
-      onSnapshot(postsQuery, (snapShot) => {
+      onSnapshot(myPostsQuery, (snapShot) => {
         let dataObj = snapShot.docs.map((doc) => ({
           ...doc.data(),
           id: doc?.id,
         }));
-        setPosts(dataObj as PostProps[]);
+        setMyPosts(dataObj as PostProps[]);
+      });
+
+      onSnapshot(likePostsQuery, (snapShot) => {
+        let dataObj = snapShot.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc?.id,
+        }));
+        setLikePosts(dataObj as PostProps[]);
       });
     }
   }, [user]);
@@ -63,18 +79,43 @@ export default function ProfilePage() {
           <div className="profile__email">{user?.email || "사용자님"}</div>
         </div>
         <div className="home__tabs">
-          <div className="home__tab home__tab--active">For You</div>
-          <div className="home__tab">Likes</div>
+          <div
+            className={`home__tab ${activeTab === "my" && "home__tab--active"}`}
+            onClick={() => setActiveTab("my")}
+          >
+            For You
+          </div>
+          <div
+            className={`home__tab ${
+              activeTab === "like" && "home__tab--active"
+            }`}
+            onClick={() => setActiveTab("like")}
+          >
+            Likes
+          </div>
         </div>
-        <div className="post">
-          {posts?.length > 0 ? (
-            posts?.map((post) => <PostBox post={post} key={post.id} />)
-          ) : (
-            <div className="post__no-posts">
-              <div className="post__text">게시물이 없습니다.</div>
-            </div>
-          )}
-        </div>
+        {activeTab === "my" && (
+          <div className="post">
+            {myPosts?.length > 0 ? (
+              myPosts?.map((post) => <PostBox post={post} key={post.id} />)
+            ) : (
+              <div className="post__no-posts">
+                <div className="post__text">게시물이 없습니다.</div>
+              </div>
+            )}
+          </div>
+        )}
+        {activeTab === "like" && (
+          <div className="post">
+            {likePosts?.length > 0 ? (
+              likePosts?.map((post) => <PostBox post={post} key={post.id} />)
+            ) : (
+              <div className="post__no-posts">
+                <div className="post__text">게시물이 없습니다.</div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
